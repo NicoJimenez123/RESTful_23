bf390bf2462d98a2c7bbabba743e56bf294bd3ad
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-09-19 23:57:17 -0300

    Añadido módulo dotenv para usar variables de entorno

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ datos que tomamos del proceso de confirguraciÃ³n de la base de datos online Mo
 const express = require('express'); // carga el módulo Express
 const mongoose = require('mongoose'); // carga el módulo Mongoose
 const Meeting = require('./models/meeting'); // carga el esquema de Mongoose que generamos en la carpeta "models"
+const dotenv = require('dotenv').config() // Cargo el módulo para usar variables de entorno
 
 const app = express(); // instancia nuetra app
+
 /*
 usa el método .conect() de Mongoose
 para conectarse a nuestra base de datos online MongoDB Altas,
@@ -16,7 +18,7 @@ pasándole como parámetro el string que nos dió la misma cuando la configuramo
 al elegir el método de conexión. Ese método .conect() devuelve una Promesa/Promise
 que resolvemos con la función .then() (... y .catch() para los posibles errores)
 */
-mongoose.connect('mongodb+srv://dBuser1:dBuser1@cluster0.schfn.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_CONNECTION_STRING) // Utilizo una variable de entorno para no exponer mi string de conexión a la BD
     .then(() => {
         console.log('Conectados a MongoDB Atlas!');
     })
